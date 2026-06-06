ec8e7b722c9918a25cf9cbd61d8955548b98cb09
Abhay Anil <abhayanil2021@gmail.com>
2026-06-07 00:24:40 +0530

    Fix CORS configuration

--- backend/main.py
+++ backend/main.py
@@ -22,13 +22,14 @@ app.add_middleware(
         "http://localhost:5173",
         "http://localhost:5174",
         "http://localhost:5175",
-        "http://localhost:5176"
+        "http://localhost:5176",
         "https://construction-dashboard-ashy.vercel.app",
     ],
+    
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-)
+    )
 
 UPLOAD_FOLDER = "backend/uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
